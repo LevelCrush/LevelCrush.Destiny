0d4667b26f97e23222181ec15f04a2fc9395d32e
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-09-02 07:48:04 -0400

    Ported DestinyComponent

---
+++  lib-destiny/Models/Schemas/DestinyComponent.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace Destiny.Models.Schemas;
+
+/// Contains component data as well as profile privacy and if it is disabled (if available)
+/// 
+/// **Source**: [Bungie Documentation](https://bungie-net.github.io/#/components/schemas/SingleComponentResponseOfDestinyProfileComponent)
+[DataContract]
+public class DestinyComponent<T> where T : class
+{
+    [DataMember(Name = "disabled", IsRequired = false)]
+    public bool? Disabled { get; set; }
+
+    [DataMember(Name = "data", IsRequired = false)]
+    public T? Data { get; set; }
+
+    [DataMember(Name = "privacy", IsRequired = false)]
+    public int Privacy { get; set; }
+}
---
+++  lib-destiny/Models/Schemas/DestinyProfileResposne.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace Destiny.Models.Schemas;
+
+/// The response for GetDestinyProfile, with components for character and item-level data.
+/// 
+/// **Note**: This does not contain all available properties of a full Destiny Profile response. Only what we need
+/// 
+/// **Source**: [Bungie Official Documentation](https://bungie-net.github.io/#/components/schemas/Destiny.Responses.DestinyProfileResponse)
+[DataContract]
+public class DestinyProfileResposne
+{
+    /// Records the timestamp of when most components were last generated from the world server source.
+    /// Unless the component type is specified in the documentation for secondaryComponentsMintedTimestamp,
+    /// this value is sufficient to do data freshness.
+    [DataMember(Name = "responseMintedTimestamp", EmitDefaultValue = true)]
+    public string TimestampResponse { get; set; }
+
+    /// Some secondary components are not tracked in the primary response timestamp and have their timestamp tracked here. If your component is any of the following, this field is where you will find your timestamp value:
+    /// 
+    /// PresentationNodes, Records, Collectibles, Metrics, StringVariables, Craftables, Transitory
+    /// 
+    /// All other component types may use the primary timestamp property.
+    [DataMember(Name = "secondaryComponentsMintedTimestamp")]
+    public string TimestampSecondaryComponents { get; set; }
+}
