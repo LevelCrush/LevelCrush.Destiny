878d9347f486f8c7b3db97bf7ff5dcd407fc4e2d
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-11-09 09:17:05 -0500

    Added skeleton for queue consumption

--- Rasputin-MessageQueue-Consumer/Program.cs
+++ Rasputin-MessageQueue-Consumer/Program.cs
@@ -1,3 +1,97 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+using System.CommandLine;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+using Rasputin.MessageQueue;
+using Rasputin.MessageQueue.Queues;
+
+
+void ConsumeMemberQueue()
+{
+    QueueMember.Subscribe(async (member) =>
+    {
+        // process
+    });
+}
+
+void ConsumeClanQueue()
+{
+    QueueClan.Subscribe(async (clan) =>
+    {
+        // process
+    });
+}
+
+void ConsumeInstanceQueue()
+{
+    QueueInstance.Subscribe(async (instance) =>
+    {
+        // process
+    });
+}
+
+// setup any cli options that we  will need
+var targetQueueOption =
+    new Option<string>(name: "--queue", description: "The target queue that this consumer will run for");
+
+
+var rootCommand = new RootCommand("Utilize Rasputin and consume in the background");
+rootCommand.AddOption(targetQueueOption);
+rootCommand.SetHandler((queue) =>
+{
+
+    var q = queue.Trim().ToLower();
+
+    switch (q)
+    {
+        case "member":
+            ConsumeMemberQueue();
+            break;
+        case "clan":
+            break;
+        case "instance":
+        default:
+            ConsumeInstanceQueue();
+            break;
+    }
+
+}, targetQueueOption);
+
+
+// run the CLI commands 
+await rootCommand.InvokeAsync(args);
+
+// handle CTRL+C 
+var exitEvent = new ManualResetEvent(false);
+Console.CancelKeyPress += (sender, e) =>
+{
+    e.Cancel = true;
+    exitEvent.Set();
+};
+
+// handle SIGINT
+PosixSignalRegistration.Create(PosixSignal.SIGINT, context =>
+{
+    context.Cancel = true;
+    exitEvent.Set();
+});
+
+// handle SIGTERM
+PosixSignalRegistration.Create(PosixSignal.SIGTERM, context =>
+{
+    context.Cancel = true;
+    exitEvent.Set();
+});
+
+
+
+// until we receive the exit event , keep waiting
+exitEvent.WaitOne();
+
+// close all connections
+// this will also close all consumers and channels automatically
+RasputinMessageQueue.Disconnect();
+
