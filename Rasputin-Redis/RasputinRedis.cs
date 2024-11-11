dfb0a4302920818c325a4b56531dd6a9e458376f
Gabriele Nunez <gabrielenunez@protonmail.com>
2024-11-11 13:22:11 -0500

    Create RasputinRedis project

---
+++ Rasputin-Redis/RasputinRedis.cs
@@ -0,0 +1,23 @@
+﻿using StackExchange.Redis;
+
+namespace Rasputin_Redis;
+
+public static class RasputinRedis
+{
+    private static readonly ConnectionMultiplexer _redis;
+
+    static RasputinRedis()
+    {
+        _redis = ConnectionMultiplexer.Connect("127.0.0.1");
+    }
+
+    public static IDatabase Connect()
+    {
+        return _redis.GetDatabase();
+    }
+
+    public static async Task Close()
+    {
+        await _redis.CloseAsync(true);
+    }
+}
---
+++ Rasputin-Redis/RasputinRedisConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Serialization;
+using Microsoft.Extensions.Configuration;
+
+namespace Rasputin_Redis;
+
+public class RasputinRedisConfig
+{
+    [DataMember] 
+    public string Host { get; set; } = "127.0.0.1";
+    
+    public static RasputinRedisConfig Load()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json", false, false)
+            .AddJsonFile("appsettings.test.json", true, false)
+            .AddJsonFile("appsettings.development.json", true, false)
+            .AddJsonFile("appsettings.staging.json", true, false)
+            .AddJsonFile("appsettings.production.json", true, false)
+            .AddJsonFile("appsettings.local.json", true, false)
+            .Build();
+
+        var section = config.GetRequiredSection("RasputinRedis");
+        var appConfig = section.Get<RasputinRedisConfig>();
+        if (appConfig == null)
+        {
+            throw new Exception("Rasputin Database configuration could not be loaded");
+        }
+
+        return appConfig;
+    }
+}
